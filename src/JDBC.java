df37cfc6e5d24ebc5aee0178952e3744d8635334
roy gebrayel <roy.gebrayel10@gmail.com>
2022-12-07 21:02:10 +0200

    created the databse JDBC also created a file that connects the java file with the MySQL database

---
+++ src/JDBC.java
@@ -0,0 +1,26 @@
+
+// file for creating a connection between the java project and the mySQL database
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC {
+   static final String DB_URL = "jdbc:mysql://localhost/JDBC";
+   static final String USER = "root";
+   static final String PASS = "NewPassword";
+
+   public static void main(String[] args) {
+      // Open a connection
+      try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+         Statement stmt = conn.createStatement();
+      ) {		      
+         String sql = "CREATE DATABASE ROY";
+         stmt.executeUpdate(sql);
+         System.out.println("Database created successfully...");   	  
+      } catch (SQLException e) {
+         e.printStackTrace();
+      } 
+   }
+}
